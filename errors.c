7f10ae3fd42785408594b980e90e4082fc255502
Omar Taraki <87668944+omar-xy@users.noreply.github.com>
2023-02-15 17:07:17 +0100

    MakeFileAutoPush

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: otaraki <otaraki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:35:34 by otaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 14:20:19 by otaraki          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:06:47 by otaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,18 @@ int error(int E)
     }
     else if(E == 2)
     {
-        write(2, "zsh: no such a file or directory\n", 34);
+        perror("second cmd Invalid");
         exit (1);
+    }
+    else if (E == 3)
+    {
+        perror("second cmd Invalid");
+        exit(1);
+    }
+    else if (E == 4)
+    {
+        write(2, "zsh: no such a file or directory\n", 34);
+        exit(1);
     }
     return (0);
 }
